2f1fe2425783f6c3118bf8dde087649aaa84c6de
RyderGarache <gar21032@byui.edu>
2021-09-26 00:40:09 -0600

    date

--- lesson2/js/last.js
+++ lesson2/js/last.js
@@ -1,6 +1,7 @@
+let oLastModif = new Date(document.lastModified)
+document.querySelector("#updated").textContent = oLastModif;  
 
-
-document.querySelector("#year").textContent = new Date().getFullYear();
-document.getElementById("last").textContent = `Last Updated:  ${document.lastModified}`;
-
-
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#year").textContent = fulldate;
