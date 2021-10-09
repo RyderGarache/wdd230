5c6d427e5656cafca456adaaf7317286df72a46c
RyderGarache <gar21032@bui.edu>
2021-10-09 01:02:12 -0600

    week4

---
+++ lesson4/js/script.js
@@ -0,0 +1,17 @@
+let oLastModif = new Date(document.lastModified)
+document.querySelector("#updated").textContent = oLastModif;  
+
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#year").textContent = fulldate;
+
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation')
+
+menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
