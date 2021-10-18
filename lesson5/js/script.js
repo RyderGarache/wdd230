647aa0eed89ba3e9c5dc7cc2be611b9c44c7b93d
RyderGarache <gar21032@bui.edu>
2021-10-17 20:58:33 -0600

    date

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,10 +1,19 @@
-let oLastModif = new Date(document.lastModified)
-document.querySelector("#updated").textContent = oLastModif;  
+// let oLastModif = new Date(document.lastModified)
+// document.querySelector("#updated").textContent = oLastModif;  
 
 const d = new Date();
 const year = d.getFullYear();
 const fulldate = `${year}`;
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+const date = d.toLocaleDateString(undefined, options);
+document.getElementById("updated").innerHTML = date;
 document.querySelector("#year").textContent = fulldate;
+
 
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation')
@@ -13,10 +22,5 @@ menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-document.querySelector(".banner_close").addEventListener("click", function () {
-  this.closest(".banner").style.display = "none";
-});
-
 
 
