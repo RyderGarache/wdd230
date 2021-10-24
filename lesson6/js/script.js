e2010ad3667f3b23d6d088a1561681a13c444244
RyderGarache <gar21032@bui.edu>
2021-10-24 01:22:53 -0600

    windchill

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -27,10 +27,4 @@ document.querySelector(".banner_close").addEventListener("click", function () {
   this.closest(".banner").style.display = "none";
 })
 
-function windChill(form) {
-wind=eval(form.wind.value);
-temp=eval(form.temp.value);
-chill=(0.0817*(3.71*(Math.pow(wind, 0.5))+
-5.81-0.25*wind)*(temp-91.4)+91.4);
-form.windchill.value = chill;
-}
+
